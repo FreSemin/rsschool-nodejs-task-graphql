cdb03c927879c841163460ae35e07dc1b51d3c46
fresemin <fresemin@gmail.com>
2024-03-02 20:37:14 +0300

    ref: index to use schema

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,16 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import {
-  GraphQLFloat,
-  GraphQLList,
-  GraphQLNonNull,
-  GraphQLObjectType,
-  GraphQLSchema,
-  GraphQLString,
-  graphql,
-} from 'graphql';
-import { Context, WithId } from './models/models.js';
-import { UUIDType } from './types/uuid.js';
+import { graphql } from 'graphql';
+import graphQLSchema from './graphql-schema.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -23,59 +14,12 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      const userType = new GraphQLObjectType({
-        name: 'User',
-        fields: () => ({
-          id: {
-            type: new GraphQLNonNull(GraphQLString),
-          },
-          name: {
-            type: GraphQLString,
-          },
-          balance: {
-            type: GraphQLFloat,
-          },
-        }),
-      });
-
-      const rootQuery = new GraphQLObjectType({
-        name: 'query',
-        fields: () => ({
-          users: {
-            type: new GraphQLList(userType),
-            resolve: async (obj, args, { prisma }: Context) => {
-              return await prisma.user.findMany();
-            },
-          },
-          user: {
-            type: userType,
-            args: {
-              id: {
-                type: new GraphQLNonNull(UUIDType),
-              },
-            },
-            resolve: async (_source, { id }: WithId, { prisma }: Context) => {
-              return await prisma.user.findUnique({
-                where: {
-                  id: id,
-                },
-              });
-            },
-          },
-        }),
-      });
-
-      const apiSchema: GraphQLSchema = new GraphQLSchema({
-        query: rootQuery,
-        types: [userType],
-      });
-
       const { query, variables } = req.body;
 
       const { prisma } = fastify;
 
       return await graphql({
-        schema: apiSchema,
+        schema: graphQLSchema,
         source: query,
         variableValues: variables,
         contextValue: {
