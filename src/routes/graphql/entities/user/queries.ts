0637ef3aefbc426a6fc6148f2c1f32c5f55b10a2
fresemin <fresemin@gmail.com>
2024-03-03 12:14:01 +0300

    ref: delete comments

--- src/routes/graphql/entities/user/queries.ts
+++ src/routes/graphql/entities/user/queries.ts
@@ -4,12 +4,10 @@ import { Context } from '../../models/context.js';
 import { UUIDType } from '../../types/uuid.js';
 import { WithId } from '../../models/args.js';
 
-// TODO: add type
 export const userQueries = {
   users: {
     type: new GraphQLList(userType),
     resolve: async (_, args, { prisma }: Context) => {
-      // TODO: move logic to service
       return await prisma.user.findMany();
     },
   },
