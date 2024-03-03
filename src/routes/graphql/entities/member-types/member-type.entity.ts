3344a1174cce5a4dcc99017799344cd900325986
fresemin <fresemin@gmail.com>
2024-03-03 15:25:48 +0300

    feat: member type entity

---
+++ src/routes/graphql/entities/member-types/member-type.entity.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '.prisma/client';
+
+export class MemberTypeEntity {
+  static async findAll(prisma: PrismaClient) {
+    return await prisma.user.findMany();
+  }
+
+  static async findOne(id: string, prisma: PrismaClient) {
+    return await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+}
