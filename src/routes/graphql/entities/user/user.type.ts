ced0ac21d3f8a63096eba855524a92f01dc84f20
fresemin <fresemin@gmail.com>
2024-03-03 20:44:13 +0300

    feat: load user profile

--- src/routes/graphql/entities/user/user.type.ts
+++ src/routes/graphql/entities/user/user.type.ts
@@ -1,7 +1,10 @@
 import { GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLFloat } from 'graphql';
 import { UUIDType } from '../../types/uuid.js';
+import { ProfileType } from '../profile/profile.type.js';
+import { ProfileService } from '../profile/profile.service.js';
+import { User } from '@prisma/client';
+import { Context } from '../../models/context.js';
 
-// TODO: rename to upperCase
 export const UserType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
@@ -13,6 +16,13 @@ export const UserType = new GraphQLObjectType({
     },
     balance: {
       type: GraphQLFloat,
+    },
+
+    profile: {
+      type: ProfileType,
+      resolve: async (user: User, args, { prisma }: Context) => {
+        return await ProfileService.findOne(user.id, prisma);
+      },
     },
   }),
 });
