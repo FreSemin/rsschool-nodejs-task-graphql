468f1b6c0d5aa122db9b9df53735cf8f0bba4df4
fresemin <fresemin@gmail.com>
2024-03-03 15:27:44 +0300

    feat: graphql queries for member type

--- src/routes/graphql/graphql-schema.ts
+++ src/routes/graphql/graphql-schema.ts
@@ -1,17 +1,20 @@
 import { GraphQLObjectType, GraphQLSchema } from 'graphql';
 import { userType } from './entities/user/user.types.js';
 import { userQueries } from './entities/user/user.queries.js';
+import { MemberType } from './entities/member-types/member-type.type.js';
+import { memberTypeQueries } from './entities/member-types/member-type.queries.js';
 
 const queries: GraphQLObjectType = new GraphQLObjectType({
   name: 'Query',
   fields: () => ({
     ...userQueries,
+    ...memberTypeQueries,
   }),
 });
 
 const graphQLSchema: GraphQLSchema = new GraphQLSchema({
   query: queries,
-  types: [userType],
+  types: [userType, MemberType],
 });
 
 export default graphQLSchema;
