e9bbdbb98cedd87a9b38010a39dbdd23878af1ad
fresemin <fresemin@gmail.com>
2024-03-04 14:53:22 +0300

    feat: get posts for user type

--- src/routes/graphql/entities/user/user.type.ts
+++ src/routes/graphql/entities/user/user.type.ts
@@ -1,9 +1,17 @@
-import { GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLFloat } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLList,
+} from 'graphql';
 import { UUIDType } from '../../types/uuid.js';
 import { ProfileType } from '../profile/profile.type.js';
 import { ProfileService } from '../profile/profile.service.js';
 import { User } from '@prisma/client';
 import { Context } from '../../models/context.js';
+import { PostType } from '../post/post.type.js';
+import { PostService } from '../post/post.service.js';
 
 export const UserType = new GraphQLObjectType({
   name: 'User',
@@ -24,6 +32,11 @@ export const UserType = new GraphQLObjectType({
         return await ProfileService.findOneByUserId(user.id, prisma);
       },
     },
+
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve: async (user: User, args, { prisma }: Context) => {
+        return await PostService.findAllByUserId(user.id, prisma);
       },
     },
   }),
