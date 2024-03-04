3e676d741092f96e017383501231594dff676d21
fresemin <fresemin@gmail.com>
2024-03-04 21:33:10 +0300

    feat: delete post mutation

--- src/routes/graphql/entities/post/post.mutations.ts
+++ src/routes/graphql/entities/post/post.mutations.ts
@@ -1,9 +1,10 @@
-import { GraphQLNonNull } from 'graphql';
+import { GraphQLBoolean, GraphQLNonNull } from 'graphql';
 import { CreatePostInput, PostType } from './post.type.js';
-import { InputDto } from '../../models/args.js';
+import { InputDto, WithId } from '../../models/args.js';
 import { Prisma } from '@prisma/client';
 import { Context } from '../../models/context.js';
 import { PostService } from './post.service.js';
+import { UUIDType } from '../../types/uuid.js';
 
 export const postMutations = {
   createPost: {
@@ -19,6 +20,20 @@ export const postMutations = {
       { prisma }: Context,
     ) => {
       return await PostService.create(dto, prisma);
+    },
+  },
+
+  deletePost: {
+    type: GraphQLBoolean,
+    args: {
+      id: {
+        type: new GraphQLNonNull(UUIDType),
+      },
+    },
+    resolve: async (_, { id }: WithId, { prisma }: Context) => {
+      await PostService.delete(id, prisma);
+
+      return null;
     },
   },
 };
